825f288a02ce574bbd3ee5919052a453c9c99d11
Branden Horiuchi <bhoriuchi@gmail.com>
2016-07-06 14:06:54 -0700

    * adding public path to webpack config

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -16,6 +16,7 @@ var webpackConfig = merge(baseWebpackConfig, {
   },
   devtool: config.build.productionSourceMap ? '#source-map' : false,
   output: {
+    publicPath: '/vue-formation',
     path: config.build.assetsRoot,
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
