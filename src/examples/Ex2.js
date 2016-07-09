9454276831fc0bf06590506a9bd2a484dfb66944
Branden Horiuchi <bhoriuchi@gmail.com>
2016-07-09 08:22:56 -0700

    * adding example

---
+++ src/examples/Ex2.js
@@ -0,0 +1,79 @@
+export default {
+  title: 'Multi-Component Form',
+  exampleType: 'formation',
+  description: 'A form that uses multiple components.',
+  headerClass: 'bg-primary',
+  formConfig: {
+    liveValidation: false,
+    rows: [
+      {
+        columns: [
+          {
+            type: 'text',
+            label: 'Text Input',
+            model: 'text'
+          },
+          {
+            type: 'fselect',
+            label: 'FSelect',
+            model: 'fselect',
+            options: [
+              { value: 'dog', text: 'Dog' },
+              { value: 'cat', text: 'Cat' },
+              { value: 'horse', text: 'Horse' },
+              { value: 'duck', text: 'Duck' },
+              { value: 'pig', text: 'Pig' },
+              { value: 'elephant', text: 'Elephant' },
+              { value: 'panda', text: 'Panda' },
+              { value: 'koala', text: 'Koala' }
+            ]
+          }
+        ]
+      },
+      {
+        columns: [
+          {
+            type: 'buttons',
+            buttons: [
+              {
+                text: 'Clear',
+                class: 'btn-default',
+                iconClass: 'glyphicon glyphicon-remove',
+                onClick (event, vm) {
+                  vm.setData('text', '')
+                  vm.setData('fselect', [])
+                }
+              },
+              {
+                text: 'Submit',
+                class: 'btn-primary',
+                iconClass: 'glyphicon glyphicon-ok',
+                onClick: (event, vm) => {
+                  if (!vm.validate()) return
+                  vm.$root.$broadcast('modal.show', {
+                    title: 'Hello!',
+                    body: vm.data,
+                    footerButtons: [
+                      {
+                        content: 'Close',
+                        class: 'btn btn-default',
+                        onClick: (event, modal) => {
+                          modal.hide()
+                        }
+                      }
+                    ]
+                  }, 'dialog')
+                }
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  formData: {
+    text: '',
+    fselect: ''
+  },
+  formHtml: '<formation :data.sync="data" :config="config">'
+}
--- src/examples/index.js
+++ src/examples/index.js
@@ -1,9 +1,11 @@
 import Ex1 from './ex1'
+import Ex2 from './Ex2'
 import TabEx1 from './TabEx1'
 import ModalEx1 from './ModalEx1'
 
 export default {
   Ex1,
+  Ex2,
   TabEx1,
   ModalEx1
 }
